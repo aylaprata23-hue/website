c86652a9c1be4195ccfedc06caac3ef3b4cc833e
aylaprata23-hue <aylaprata23@gmail.com>
2025-10-20 08:22:10 -0300

    adicionando os arquivos

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function mudar_texto_elemento() {
 
+  document.getElementById("demo").innerHTML = "O clique está funcionando"
+
+}
+
+function mudar_cor_elemento(){
+
+  const paragrafo = document.querySelector('#paragrafo');
+
+  paragrafo.addEventListener('click', ()=> {
+
+    const elemento_paragrafo = document.querySelector('#paragrafo');
+
+    elemento_paragrafo.style.color = 'red';
+
+  });
+
+}
+
+function mudar_estilo_textual(){
+
+  document.getElementById("elemento_strong").style.textDecorationLine = "line-through";
+}
